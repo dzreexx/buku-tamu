97e2b56a11733dc480c6d825213f458c4787f754
RajaNeptunus <rajaneptunus@RajaNeptunuss-MacBook-Pro.local>
2024-05-13 14:05:29 +0700

    setup mac

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite('resources/css/app.css')
 </head>
 <body>
     <button class="btn" onclick="my_modal_3.showModal()">CLICK ME</button>
@@ -18,5 +17,6 @@
     <p class="py-4">Press ESC key or click on ✕ button to close</p>
   </div>
 </dialog>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.6086906317514!2d106.88462177446873!3d-6.315023793674334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ed670184f525%3A0xc29ced5182e97ec0!2sTRAINER%20PRIVATE%20COURSE%20dan%20menerima%20Service%20Mac%20-%20Laptop%20Gaming%2FStandart!5e0!3m2!1sid!2sid!4v1714793904783!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </body>
 </html>
